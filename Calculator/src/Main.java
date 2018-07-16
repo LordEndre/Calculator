c90e7b9a7c04b1bc8cc50ee39bf8891c12cb136a
lordendre <lordendre@LordEndre-Arch>
2018-07-16 14:47:51 +0200

    First commit

---
+++ Calculator/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Calculator");
+		
+		
+		System.out.println("Press a number");
+		int i = scanner.nextInt();
+		
+		
+		
+		
+		System.out.println(i);
+		
+		
+		
+		scanner.close();
+		
+		}
+	
+
+	}
