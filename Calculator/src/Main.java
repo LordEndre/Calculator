66e7c8810ce8d0880c402e1d304b232614e09860
lordendre <lordendre@LordEndre-Arch>
2018-07-16 15:11:23 +0200

    Adding addition.

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -5,20 +5,41 @@ public class Main {
 	public static void main (String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		boolean run = true;
 		
 		System.out.println("Calculator");
 		
 		
-		System.out.println("Press a number");
-		int i = scanner.nextInt();
+		while (run) {
+			
+			System.out.println("Press a number");
+			System.out.println("1, Add two numbers");
+			System.out.println("5, exit");
+			int in = scanner.nextInt();	
+			
+			if(in==1) {
+				System.out.println("Enter the first number");
+				int a = scanner.nextInt();
+				System.out.println("Enter the first number");
+				int b = scanner.nextInt();
+				System.out.println(a + "+" + b + "=" + Addition.add(a, b));
+				}
+			
+			
+			
+			
+			if(in == 5) {
+				run = false;
+				break;
+				}
+			
+			
+			
+			}
 		
 		
-		
-		
-		System.out.println(i);
-		
-		
-		
+		System.out.println("Goodbye :)");
+	
 		scanner.close();
 		
 		}
